eb29ba48b7680f28d8ef5cf12b1c1993958934ad
xIvan0ff <hrisivanov2@gmail.com>
2022-03-07 18:50:15 +0200

    Updated the installer script.

--- installer.sh
+++ installer.sh
@@ -4,6 +4,11 @@ then
     curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh && sudo bash nodesource_setup.sh && sudo apt install nodejs
 fi
 
-rm -rf *
-git clone https://github.com/xIvan0ff/ServerManager.git .
-bash updater.sh
+git clone https://github.com/xIvan0ff/ServerManager.git
+rm -rf client
+mkdir client
+mv ServerManager/client/* client/
+rm -rf ServerManager
+cd client
+npm install
+screen node main.js
--- updater.sh
+++
@@ -1,6 +0,0 @@
-# cd ServerManager
-git pull
-rm -rf server/*
-cd client
-npm install
-screen node main.js
